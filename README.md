86fffbbaa626cfabe7c96b3f54394a5413d75c6c
Brian Bassanetti <89162244+BrainTheBest5@users.noreply.github.com>
2022-12-11 15:36:01 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Variables
  
+A little program where I had to play with variables. It was difficult to come up with a program that utilised all variable types at once
---
+++ Variables.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*https://www.tutorialspoint.com/convert-an-int-to-ascii-character-in-c-cplusplus
+Senza questa piccola guida non avrei saputo come combinare i tre tipi di variabili in un unico programma perché non trovavo un problema da risolvere, ed essendo un programma la soluzione ad un problema....*/
+int num2char(int num){
+	return 0 + num;
+}
+int main(){
+	printf("Inserire un numero con la virgola: ");
+	int A;
+	scanf("%d",&A);
+	float B;
+	scanf("%f.2",&B);
+	printf("La parte intera corrsiponde a %d\n",A);
+	printf("La parte deciamle corrisponde a %f\n",B);
+	printf("Il numero che hai inserito nella codifica ASCII corrisponde al carattere %c\n",num2char(A));
+	return 0;
+}
